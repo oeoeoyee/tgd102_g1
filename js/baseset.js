ffae3fcaaaa8d617e4cc471c4c62b563217ce6f9
stranger583 <zed042760@gmail.com>
2022-09-23 13:20:49 +0800

    0923_修改後台編輯欄位

--- js/baseset.js
+++ js/baseset.js
@@ -19,44 +19,44 @@ $(function () {
 // });
 
 // vuevue 到頂的按鈕 
-// Vue.directive('scroll',{
-//     bind:function(el,binding){
-//       window.addEventListener('scroll',()=>{          
-//         let fnc = binding.value;
-//         fnc(el);
-//       })
-//     }
-// })
+Vue.directive('scroll',{
+    bind:function(el,binding){
+      window.addEventListener('scroll',()=>{          
+        let fnc = binding.value;
+        fnc(el);
+      })
+    }
+});
 
-// var totop_vm = new Vue({
-//     el: "#app_totop",
-//     data:{
-//       goTop: false,
-//       scrollTop: ""
-//     },
-//     methods:{        
-//       gotop: function(){          
-//         let speed = 10;
-//         let timer = setInterval(function(){            
-//           this.scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-//           if(this.scrollTop>0){
-//             this.scrollTop = (this.scrollTop - speed >0) ? (this.scrollTop - speed) : 0;
-//             speed += 20;
-//             window.scrollTo(0,this.scrollTop);
-//           }else{
-//             clearInterval(timer);
-//           }
-//         },16)
-//       },
-//       showTop: function(){          
-//         // this.scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-//         this.scrollTop = window.pageYOffset;
-//         if(this.scrollTop>200){
-//             this.goTop = true;
-//         }else{
-//             this.goTop = false;
-//         }
-//       }
+var totop_vm = new Vue({
+    el: "#app_totop",
+    data:{
+      goTop: false,
+      scrollTop: ""
+    },
+    methods:{        
+      gotop: function(){          
+        let speed = 10;
+        let timer = setInterval(function(){            
+          this.scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+          if(this.scrollTop>0){
+            this.scrollTop = (this.scrollTop - speed >0) ? (this.scrollTop - speed) : 0;
+            speed += 20;
+            window.scrollTo(0,this.scrollTop);
+          }else{
+            clearInterval(timer);
+          }
+        },16)
+      },
+      showTop: function(){          
+        // this.scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+        this.scrollTop = window.pageYOffset;
+        if(this.scrollTop>200){
+            this.goTop = true;
+        }else{
+            this.goTop = false;
+        }
+      }
 
-//     }
-// });
+    }
+});
