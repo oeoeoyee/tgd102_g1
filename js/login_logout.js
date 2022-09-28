4c213817af208c3d6acd56adf01cc5be2ff2ebcf
NekoCheese <happaite4664@gmail.com>
2022-09-28 19:57:42 +0800

    0928_登入狀態_雖然只是網頁_但可不是鬧著玩的(無法登出)

--- js/login_logout.js
+++ js/login_logout.js
@@ -1,10 +1,11 @@
 // 登入功能
 // 先嘗試綁定 沒有綁定到就跳過 try catch
+
 (() => {
   // 取欄位值
-  const useremail = document.querySelector("#email");
-  const password = document.querySelector("#pwd");
-  const errMsg = document.querySelector("#error");
+  const useremail = document.querySelector("#email"); // 信箱 輸入欄
+  const password = document.querySelector("#pwd"); // 密碼 輸入欄
+  const errMsg = document.querySelector("#error"); // 錯誤訊息
 
   // 1. 綁定 id="login_btn" 加入登入功能
   try {
@@ -28,8 +29,8 @@
             sessionStorage.setItem("MEMBER_ID", MEMBER_ID);
             sessionStorage.setItem("NAME", NAME);
             sessionStorage.setItem("EMAIL", EMAIL);
-            // 成功後轉址回首頁
-            location = "./index.html";
+            // 成功後 轉址 ? 會員中心
+            location = "./member_info.html";
           } else {
             // 失敗將收到的錯誤訊息寫進 HTML
             errMsg.textContent = message;
@@ -46,38 +47,52 @@
   const MEMBER_ID = sessionStorage.getItem("MEMBER_ID");
   const NAME = sessionStorage.getItem("NAME");
   const EMAIL = sessionStorage.getItem("EMAIL");
+
   console.log(MEMBER_ID, NAME, EMAIL);
-
   // 綁定
+  // 登入狀態 ( 顏色 )
+  const user_avatar = document.querySelector("#user_avatar");
+  // "有" 會員資料 的功能
+  const my_info = document.querySelector("#my_info"); // 會員資料
+  const my_order = document.querySelector("#my_order"); // 訂單紀錄
+  const my_level = document.querySelector("#my_level"); // 等級
+  const logout = document.querySelector("#logout"); // 登出
 
-  const register = document.querySelector("#register");
-  const edit = document.querySelector("#edit");
-  const manage = document.querySelector("#manage");
-  const login = document.querySelector("#login");
-  const logout = document.querySelector("#logout");
+  // "無" 會員資料 的功能
+  const register = document.querySelector("#register"); // 註冊
+  const login = document.querySelector("#login"); // 登入
 
   if (NAME) {
-    console.log("HI," + NAME);
+    user_avatar.classList.add("loging");
+    // 添加 class ( display none ) 隱藏 註冊、登入
+    register.classList.add("hide");
+    login.classList.add("hide");
 
-    // register.classList.add("hide");
-    // edit.classList.remove("hide");
+    // 移除 class ( 不 none ) 顯示會員功能
+    my_info.classList.remove("hide");
+    my_order.classList.remove("hide");
+    my_level.classList.remove("hide");
+    logout.classList.remove("hide");
+
     // roleId == 1
     //   ? manage.classList.remove("hide")
     //   : manage.classList.add("hide");
-    // login.classList.add("hide");
-    // logout.classList.remove("hide");
     // document.querySelector("#currentUser").textContent = nickname;
-    // document.querySelector("#avatar").src = URL.createObjectURL(
-    //   base64StrToBlob(avatar)
-    // );
   } else {
-    console.log("NO");
+    // 沒有 sessionStorage 資料
+    
+    // 添加 class ( display none ) 隱藏 會員功能
+    my_info.classList.add("hide");
+    my_order.classList.add("hide");
+    my_level.classList.add("hide");
+    logout.classList.add("hide");
 
-    // register.classList.remove("hide");
-    // edit.classList.add("hide");
-    // manage.classList.add("hide");
-    // login.classList.remove("hide");
-    // logout.classList.add("hide");
+    // 移除 class ( 不 none ) 顯示 註冊、登入
+    register.classList.remove("hide");
+    login.classList.remove("hide");
+    
+    // 登入狀態顏色
+    user_avatar.classList.remove("loging");
   }
 
   // 回到前面的路徑 (功能尚未明確)
@@ -88,13 +103,13 @@
     );
   }
 
-  // 登出鍵
+  // 登出鍵 功能不完全
   try {
     // 嘗試綁定
     logout.addEventListener("click", () => {
       // 移除 sessionStorage NAME 值
-      // sessionStorage.removeItem("NAME");
-      // fetch("member/logout");
+      sessionStorage.removeItem("NAME");
+      fetch("member/logout");
       location = `${getContextPath()}/tgd102_g1/dist/index.html`;
     });
   } catch (error) {}
