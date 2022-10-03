e431625ff4bbf5c0a6d65cdd57bdfe7d26fd804b
stranger583 <zed042760@gmail.com>
2022-10-03 17:33:52 +0800

    1003_add_忘記密碼

--- js/back_events_edit.js
+++ js/back_events_edit.js
@@ -1,6 +1,7 @@
 new Vue({
     el:"#app",
     data:{
+        ID:"",
         eng_title:"",
         title:"",
         date:"",
@@ -15,9 +16,9 @@ new Vue({
              "D",
              "E",
         ],
-        main_img:"",
+        main_img:"2張圖片",
         main_content:"",
-        events_img:"",
+        events_img:"2張圖片",
         events_title:"",
         events_content:"",
         artist_title:"",
@@ -36,16 +37,56 @@ new Vue({
 
     },
     mounted(){
+        let that = this;
+        let getUrlString = location.href;
+        let url = new URL(getUrlString);
+        let newsID = url.searchParams.get('id'); //抓id
+        if(newsID !== null){
+            fetch(`./php/back_events_edit_select.php?id=`+newsID,{
+                method: 'POST', 
+                headers: {'Content-Type':'application/json'}, 
+                body: JSON.stringify({
+                ID: newsID,//這是幹嘛用的
+                })
+            })
+            .then(resp => resp.json())
+            .then((info)=>{
+                that.ID = info[0].ID,
+                that.eng_title = info[0].eng_title,
+                that.title = info[0].title,
+                that.start_day = info[0].start_day,
+                that.end_day = info[0].end_day,
+                that.room = info[0].room,
+                that.main_img = info[0].main_img,
+                that.main_content = info[0].main_content,
+                that.events_img = info[0].events_img,
+                that.events_title = info[0].events_title,
+                that.events_content = info[0].events_content,
+                that.artist_img = info[0].artist_img,
+                that.artist_title = info[0].artist_title,
+                that.artist_content = info[0].artist_content,
+                that.pj_title1 = info[0].pj_title1,
+                that.pj_img1 = info[0].pj_img1,
+                that.pj_content1 = info[0].pj_content1,
+                that.pj_title2 = info[0].pj_title2,
+                that.pj_img2 = info[0].pj_img2,
+                that.pj_content2 = info[0].pj_content2,
+                that.pj_title3 = info[0].pj_title3,
+                that.pj_img3 = info[0].pj_img3,
+                that.pj_content3 = info[0].pj_content3,
+                that.situation =info[0].situation
+            })
+        };
     },
     methods:{
         select1(){
             this.room = document.getElementById("type").value;
         },
         getFiles1(e){
-            this.main_img = e.target.files[0].name ;
+            this.main_img = e.target.files[0].name +'|'+ e.target.files[1].name;
         },
         getFiles2(e){
-            this.events_img = e.target.files[0].name +'|'+ e.target.files[1].name
+            this.events_img = e.target.files[0].name +'|'+ e.target.files[1].name;
         },
         getFiles3(e){
             this.artist_img = e.target.files[0].name 
@@ -70,6 +111,7 @@ new Vue({
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
+                    ID:this.ID,
                     eng_title: this.eng_title,
                     title: this.title,
                     start_day: this.start_day,
@@ -93,15 +135,13 @@ new Vue({
                     pj_img3: this.pj_img3,
                     pj_content3: this.pj_content3,
                     situation:this.situation,
-    
-    
                 }),
             })
             // .then(resp => resp.json())
             .then(body => {
                 console.log(body);
                 if(body !=""){
-                location = e
+                // location = e
                 alert("發送成功");
                 }
             });
--- php/events.php
+++ php/events.php
@@ -2,7 +2,7 @@
 
 include("./PDO/connection_inc.php");
        //建立SQL語法
-       $sql = "SELECT * FROM EVENTS_TEST";
+       $sql = "SELECT * FROM EVENTS_TEST ORDER by start_date desc";
 
        //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
        $statement = $pdo->prepare($sql);
