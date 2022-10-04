becb697f457ac3d4d5d054b675f2d05fdd28be4e
NekoCheese <happaite4664@gmail.com>
2022-10-04 20:20:59 +0800

    1004_拿掉登入預設值_普展內頁內容增加_感謝浩宇感謝浩宇感謝浩宇!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- php/PDO/connection_inc.php
+++ php/PDO/connection_inc.php
@@ -11,7 +11,7 @@ $pdo = new PDO($URL, $USERNAME, $PASSWORD);
 //    校外開發環境 ( 需建立個人電腦端測試用 DB [REVERSE_TEST] ) 
 // -----------------------------------------
 // $URL = "mysql:host=localhost;dbname=REVERSE_TEST";
-// // $URL = "mysql:host=localhost;dbname=REVERSE";
+// $URL = "mysql:host=localhost;dbname=REVERSE";
 // $USERNAME = "root";
 // $PASSWORD = "password";
 // $pdo = new PDO($URL, $USERNAME, $PASSWORD);
